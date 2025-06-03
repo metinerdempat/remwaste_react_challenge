c32effbcb2e5fa3d66550ac79a6ffcad838dbb1b
metinerdempat <metinerdempatweb@gmail.com>
2025-06-03 23:26:43 +0300

    feat: add zustand stores for steps

---
+++ src/stores/use-steps.ts
@@ -0,0 +1,27 @@
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+
+interface StepsState {
+  stepId: number;
+  data: unknown;
+  /* doesn't need setStepId because of just simulating the stepper */
+  // setStepId: (stepId: number) => void;
+  setData: (data: unknown) => void;
+}
+
+const useStepsStore = create<StepsState>()(
+  persist(
+    (set) => ({
+      stepId: 3,
+      data: null,
+      setStepId: (stepId: number) => set({ stepId }),
+      setData: (data: unknown) => set({ data }),
+    }),
+    {
+      name: '@remwaste/steps',
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+);
+
+export default useStepsStore;
